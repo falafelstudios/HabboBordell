aa81e976fe9652fc3e46a332a103e5df10c758b6
falafelstudios <32414681+falafelstudios@users.noreply.github.com>
2018-06-08 13:19:46 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-//Saker
-const commando = require("discord.js-commando");
-const bot = new commando.Client();
-bot.login("NDUyMTQ1ODQwMTgyNzg4MTA2.DfMGNQ.r0YB738daCMSISirBRJ-RmPGXD8");
-owner: '452815382722379776',
-// a b ø w 
-bot.registry.registerGroup('bordell', 'Random');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands");
